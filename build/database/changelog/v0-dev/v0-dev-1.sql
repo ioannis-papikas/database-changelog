da89a9aeb8cc81df4ec1e0d6a9d087d97b774963
Ioannis Papikas <papikas.ioan@gmail.com>
2017-08-08 10:57:12 +0300

    Remove initial snapshot and move to first database change
    Support existing databases and instead of setting up an invalid
    initial snapshot, set the first change to create the version history
    table

--- build/database/changelog/v0-dev/v0-dev-1.sql
+++ build/database/changelog/v0-dev/v0-dev-1.sql
@@ -1,14 +1,27 @@
+--
+-- Table structure for table `version_history`
+--
+
+CREATE TABLE `version_history` (
+  `version_id` varchar(50) NOT NULL,
+  `started_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `finished_on` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `version_history`
+--
+ALTER TABLE `version_history`
+  ADD PRIMARY KEY (`version_id`);
+
+
 -- INSERT VERSION HISTORY
 SET @version := 'v0-dev-1';
 INSERT INTO `version_history` (`version_id`, `started_on`) VALUES (@version, CURRENT_TIMESTAMP());
-
-
--- -------------------------------------------------------
--- -------------------------------------------------------
--- ADD YOUR SQL CODE BELOW THIS LINE
--- -------------------------------------------------------
-
--- REPLACE THIS LINE WITH YOUR SQL CHANGES --
 
 -- FINALIZE VERSION HISTORY
 UPDATE `version_history` SET `finished_on` = CURRENT_TIMESTAMP() WHERE `version_id` = @version;
